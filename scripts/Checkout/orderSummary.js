5e8a1ceb63a821df9f780b3680efd26741c6ac3b
Rajat Pratap Singh <168651126+Rajat-Chauhan152@users.noreply.github.com>
2024-06-30 12:18:15 +0530

    Update orderSummary.js

--- scripts/Checkout/orderSummary.js
+++ scripts/Checkout/orderSummary.js
@@ -58,6 +58,7 @@ export function renderOrderSummary() {
                 </div>
                 ${deliveryOptionsHTML(matchingProduct, cartItem)}
             </div>
+            </div>
         </div>
         `;
     });
